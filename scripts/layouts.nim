7fb1c399d3d061fefecf0feee0da9b38e73197cf
Eichi-Arakaki <justanunusualperson@gmail.com>
2022-01-14 23:14:38 +0900

    IndexDefect and Settings.png changed

--- scripts/layouts.nim
+++ scripts/layouts.nim
@@ -10,24 +10,25 @@ import
 const COMMAND = "setxkbmap"
 let KEYBOARDS: seq[string] = @["us", "es"]
 
-proc next_layout(): string =
+proc nextLayout(): string =
     let current_l: string = execCmdEx(fmt"{KEYBOARD_SCRIPT()} print %e").output
     var next_l: string
 
     try:
         next_l = KEYBOARDS[KEYBOARDS.find(current_l.strip()) + 1]
-    except:
+    except IndexDefect:
         next_l = KEYBOARDS[0]
     finally:
         return next_l
 
 
-let nl = next_layout()
+let nl = nextLayout()
 shell:
     ($COMMAND) ($nl)
+
 notifySend(
     title = "Keyboard Notification", 
     subtitle = fmt"Layout: {nl.toUpperAscii}", 
     time_out = 1000,
-    icon = QTILE_PATH / "icons" / "SettingsOrange.png"
+    icon = QTILE_PATH / "icons" / "SettingsBlue.png"
 )
